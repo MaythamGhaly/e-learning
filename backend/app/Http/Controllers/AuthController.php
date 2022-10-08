38f6039e92995d9ce8178ea8df20976f04ce5e2d
Maytham Ghaly <maythamghaly@gmail.com>
2022-10-08 22:22:33 +0300

    add register and login

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -1,22 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Tymon\JWTAuth\Facades\JWTAuth;
 use App\Models\User;
 
 class AuthController extends Controller
-{ 
-    public function register(){
+{
+    public function register()
+    {
 
-        $validator = validator()->make(request()->all(),[
-            'name'=>'string|required',
-            'email'=>'email|required',
-            'password'=>'string|required',
-            'user_type'=>'string|required',
+        $validator = validator()->make(request()->all(), [
+            'name' => 'string|required',
+            'email' => 'email|required',
+            'password' => 'string|required',
+            'user_type' => 'string|required',
 
         ]);
-        if ($validator->fails()){
+        if ($validator->fails()) {
             return response()->json([
                 'message' => 'registration faild!'
             ]);
@@ -24,38 +27,35 @@ class AuthController extends Controller
         $user = User::create([
             'name' => request()->get('name'),
             'email' => request()->get('email'),
-            'password'=>bcrypt(request()->get('password')),
-            'user_type'=>request()->get('gender'),
+            'password' => bcrypt(request()->get('password')),
+            'user_type' => request()->get('user_type'),
 
         ]);
-        return response()-> json([
-            'message'=>'User Created!',
-            'user'=>$user
+        return response()->json([
+            'message' => 'User Created!',
+            'user' => $user
         ]);
     }
 
-   public function login()
-   {
-       $credentials = request(['email', 'password']);
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
 
-       if (! $token = auth()->attempt($credentials)) {
-           return response()->json(['error' => 'Unauthorized'], 401);
-       }
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
 
-       return $this->respondWithToken($token);
-   }
+        return $this->respondWithToken($token);
+    }
 
-   public function me()
-   {
-       return response()->json(auth()->user());
-   }
-
-   protected function respondWithToken($token)
-   {
-       return response()->json([
-           'access_token' => $token,
-           'token_type' => 'bearer',
-           'expires_in' => config('jwt.ttl')
-       ]);
-   }
+    protected function respondWithToken($token)
+    {
+        $user = auth::user();
+        return response()->json([
+            'access_token' => $token,
+            'user' => $user,
+            'token_type' => 'bearer',
+            'expires_in' => config('jwt.ttl')
+        ]);
+    }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,5 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Auth;
 
 
+
+
+
+Route::post('/register', [AuthController::class, "register"])->name("register");
+Route::post('/login', [AuthController::class, "login"])->name("login");
