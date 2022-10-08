862866439b91a6a7e0e82b49e0b19cc5fe870604
Maytham Ghaly <maythamghaly@gmail.com>
2022-10-08 22:28:53 +0300

    add middleware

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,9 +5,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Auth;
 
+Route::group(['middleware' => 'user.role'], function () {
 
+    Route::post('/logout', [AuthController::class, "logout"])->name("logout");
 
-
+});
 
 Route::post('/register', [AuthController::class, "register"])->name("register");
 Route::post('/login', [AuthController::class, "login"])->name("login");
