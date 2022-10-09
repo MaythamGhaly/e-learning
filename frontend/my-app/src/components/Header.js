f2523d60b1e27cdfa01ff4579aedf8117ea0c417
Maytham Ghaly <maythamghaly@gmail.com>
2022-10-09 17:50:26 +0300

    frontend: add header component

---
+++ frontend/my-app/src/components/Header.js
@@ -0,0 +1,17 @@
+import { useLocation } from "react-router-dom";
+
+export const Header = ({ title }) => {
+  const location = useLocation();
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      {location.pathname === "/"}
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Login",
+};
+
+export default Header;
