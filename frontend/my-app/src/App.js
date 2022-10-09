341e3b2671c451334f284c2b622c6a2c55f02430
Maytham Ghaly <maythamghaly@gmail.com>
2022-10-09 17:50:59 +0300

    frontend: add login function

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,28 +1,34 @@
 import { useState, useEffect } from "react";
+import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Header} from "./components/Header"
+import Login from "./components/Login"
+import axios from "axios";
+
 import './App.css';
 
 function App() {
-  const [email, setemail] = useState([]);
 
+  const login = async ( user) => {
+    let url=`http://127.0.0.1:8000/api/login`
+    const data ={
+      email: user.email,
+      password: user.password
+    }
+    const response =  await axios.post( url, data )
+      localStorage.setItem("user" ,response.data.user._id, "token", response.data.access_token)
+    
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Header/>
+        <Routes>
+          <Route path="/" element={ <Login onLogin={ login } />}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
