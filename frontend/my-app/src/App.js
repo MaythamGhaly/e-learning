8c1d9cec751984bdacea6c2bdbcbb9a39a83784d
Maytham Ghaly <maythamghaly@gmail.com>
2022-10-10 13:07:06 +0300

    frontend: add admin page

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {Header} from "./components/Header"
+import { Header } from "./components/Header"
 import Login from "./components/Login"
 import axios from "axios";
 
@@ -9,23 +9,35 @@ import './App.css';
 
 function App() {
 
-  const login = async ( user) => {
-    let url=`http://127.0.0.1:8000/api/login`
-    const data ={
+  const login = async (user) => {
+    let url = `http://127.0.0.1:8000/api/login`
+    const data = {
       email: user.email,
       password: user.password
     }
-    const response =  await axios.post( url, data )
-      localStorage.setItem("user" ,response.data.user._id, "token", response.data.access_token)
-    
+    const response = await axios.post(url, data)
+      .catch(function (error) {
+        alert("incorrect email or password")
+      });
+    localStorage.setItem("token", response.data.access_token)
+    localStorage.setItem("user", response.data.user._id)
+    console.log(response.data.user.user_type)
+    if (response.data.user.user_type == "admin") {
+      window.location.href = 'http://localhost:3000/admin'
+    } else if (response.data.user.user_type == "instructor") {
+      window.location.href = 'http://localhost:3000/instructor'
+    }
+    window.location.href = 'http://localhost:3000/student'
+
   }
 
   return (
     <BrowserRouter>
       <div className="container">
-        <Header/>
+        <Header />
         <Routes>
-          <Route path="/" element={ <Login onLogin={ login } />}/>
+          <Route path="/login" element={<Login onLogin={login} />} />
+          <Route path="/admin" element={<Admin onAddUser={register} />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ frontend/my-app/src/components/Admin.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+const Admin = ({ onLogin }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const onSubmit = (e) => {
+      e.preventDefault();
+
+      if(!email || !password){
+        alert("email or password incorrect")
+        return
+      }
+  
+      onLogin({ email, password });
+  
+      setEmail("");
+      setPassword("");
+    };
+    return (
+        <>
+            <form className="flex center login-container" onSubmit={onSubmit}>
+                <div className="login-section">
+                    <input
+                        className="login-input"
+                        type="email"
+                        placeholder="Enter your email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        className="login-input"
+                        type="password"
+                        placeholder="Enter your password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                    <input type={"submit"} value="Login" className="login-btn" />
+                    
+                </div>
+            </form>
+        </>
+    )
+}
+
+export default Login
