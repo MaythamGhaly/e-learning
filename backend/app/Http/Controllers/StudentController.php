a39ac64c1c6c76df5e480fc0e818871e2949a851
Maytham Ghaly <maythamghaly@gmail.com>
2022-10-11 14:09:50 +0300

    backend: finish add courses register

---
+++ backend/app/Http/Controllers/StudentController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CoursRegister;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class StudentController extends Controller
+{
+    public function registeCourses()
+    {
+        $id=auth::id();
+        $Coursregister = new Coursregister;
+        $cours_name=request()->cours_name;
+        $student_id=request()->student_id;
+        $Coursregister->cours_name = $cours_name;
+        $Coursregister->student_id = $id;
+        
+        
+        if ($Coursregister->save()) {
+            return response()->json([
+                'status' => 'success'
+            ]);
+        }
+        return response()->json([
+            'status' => 'failed'
+        ], 401);
+    }
+}
